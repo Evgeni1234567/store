2cf7c14b7273f5aa6bcbea9c1c0d0cf249701849
Evgeni <valushkoevgeni@gmail.com>
2021-05-28 00:08:00 +0300

    добавили реализацию  репозитория книг в пямять

---
+++ Infrastructure/StoreMemory/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Store;
+using System.Linq;
+
+namespace StoreMemory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book (1, "Art of Programming"),
+            new Book (2, "Refact"),
+            new Book (3, "C prog")
+        
+        };
+
+        public Book[] GetByTitle(string titlePatr)
+        {
+            return books.Where(book => book.Title.Contains(titlePatr))
+                        .ToArray();
+        }
+    }
+}
