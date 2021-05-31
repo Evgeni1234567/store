0cc664cd51f9b91f6e4fe69aa413d473dd233f75
Evgeni <valushkoevgeni@gmail.com>
2021-05-31 23:48:54 +0300

    передобавил

---
+++ domain/Store/BookService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class BookService
+    {
+        public Book[] GetAllByQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
